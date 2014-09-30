7b11c30d7d429cb9337dc00b426e4c61a70763bb
Xiang Yu <eggplant.elf@gmail.com>
2014-09-30 15:16:36 +0200

    separate training and test mode

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, time
+import sys, time, argparse
 from model import *
 from sentence import *
 from parser import *
@@ -7,7 +7,16 @@ from labeler import *
 
 import cProfile
 
-def test(conll_file, parser, labeler, output_file):
+
+def train(train_file, parser_model_file, labeler_model_file): 
+    parser = Parser()
+    labeler = Labeler()
+    parser.train(train_file, parser_model_file)
+    labeler.train(train_file, labeler_model_file)
+
+def test(conll_file, parser_model_file, labeler_model_file, output_file):
+    parser = Parser(parser_model_file)
+    labeler = Labeler(labeler_model_file)
     outstream = open(output_file,'w')
     for sent in read_sentence(open(conll_file)):
         parser.predict(sent)
@@ -15,64 +24,22 @@ def test(conll_file, parser, labeler, output_file):
         print >> outstream, sent.to_str()
     outstream.close()
 
-def evaluate(conll_file):
-    total = 0.0
-    correct_head = 0.0
-    correct_label = 0.0
-    for line in open(conll_file):
-        if line.strip():
-            items = line.strip().split('\t')
-            total += 1
-            if items[6] == items[8]:
-                correct_head += 1
-                if items[7] == items[9]:
-                    correct_label += 1
-    print 'UAS: %6.2f%%' % (100 * correct_head/total)
-    print 'LAS: %6.2f%%' % (100 * correct_label/total)
-
-def graph_demo(argv):
-    train_file = argv[1]
-    parser_model_file = argv[2]
-    labeler_model_file = argv[3]
-    test_file = argv[4]
-    output_file = argv[5]
- 
-    t0 = time.time()
-    parser = Parser()
-    labeler = Labeler()
-    parser.train(train_file, parser_model_file)
-    labeler.train(train_file, labeler_model_file)
-
-    parser = Parser(parser_model_file)
-    labeler = Labeler(labeler_model_file)
-    test(test_file, parser, labeler, output_file)
-    # evaluate(output_file)
-    print 'time used:', time.time() - t0
-
-
-def easy_demo(argv):
-    train_file = argv[1]
-    parser_model_file = argv[2]
-    labeler_model_file = argv[3]
-    test_file = argv[4]
-    output_file = argv[5]
-    max_dist = int(argv[6])
-
-    t0 = time.time()
-    parser = EasyFirstParser(max_dist = max_dist)
-    parser.train(train_file, parser_model_file, 10)
-
-    labeler = Labeler()
-    labeler.train(train_file, labeler_model_file)
-
-    parser = EasyFirstParser(parser_model_file, max_dist)
-    labeler = Labeler(labeler_model_file)
-    test(test_file, parser, labeler, output_file)
-    evaluate(output_file)
-    print 'time used:', time.time() - t0
-
 ####################################################
 
 if __name__ == '__main__':
-    # cProfile.run('graph_demo(sys.argv)')
-    graph_demo(sys.argv)
+    argpar = argparse.ArgumentParser(description='Parser')
+    mode = argpar.add_mutually_exclusive_group(required=True)
+    mode.add_argument('-train',dest='train',action='store_true',help='run in training mode')
+    mode.add_argument('-test',dest='test',action='store_true',help='run in test mode')
+    argpar.add_argument('-t','--trainingfile',dest='trainingfile',help='training file',required=False)
+    argpar.add_argument('-p','--parser',dest='parser',help='parser model',required=True)
+    argpar.add_argument('-l','--labeler',dest='labeler',help='labeler model',required=True)
+    argpar.add_argument('-i','--input',dest='inputfile',help='input file',required=False)
+    argpar.add_argument('-o','--output',dest='outputfile',help='output file',default='output.conll06', required=False)
+
+    args = argpar.parse_args()
+
+    if args.train:
+        train(args.trainingfile,args.parser,args.labeler)
+    elif args.test:
+        test(args.inputfile, args.parser,args.labeler, args.outputfile)
