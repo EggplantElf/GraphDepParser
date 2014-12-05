b4a14821a4ca9aadcaa4a5c3cff4144bb5536087
Xiang Yu <eggplant.elf@gmail.com>
2014-12-06 00:40:38 +0100

    ..

--- cross_test.sh
+++ cross_test.sh
@@ -1,47 +1,47 @@
 
 
 
-# for g in answer email newsgroup weblogs reviews
-# do
+for g in answer email newsgroup weblogs reviews
+do
 
-#     train_file=../tmp/web/train_$g.cx
+    train_file=../tmp/web/train_$g.cx
 
-#     sent_parser=../tmp/web/sent.$g.parser
-#     IOB_sent_parser=../tmp/web/IOB_sent.$g.parser
-#     chunk_parser=../tmp/web/chunk.$g.parser
-#     clause_parser=../tmp/web/clause.$g.parser
-#     chunk_sent_parser=../tmp/web/chunk_sent.$g.parser
-#     clause_sent_parser=../tmp/web/clause_sent.$g.parser
-
-
-
-#     test_file=../tmp/web/$g.cx
-#     baseline_output=../tmp/web/$g.baseline.conll06
-#     IOB_output=../tmp/web/$g.IOB.conll06
-#     chunk_output=../tmp/web/$g.chunk.conll06
-#     clause_output=../tmp/web/$g.clause.conll06
-#     echo $g
+    sent_parser=../tmp/web/sent.$g.parser
+    IOB_sent_parser=../tmp/web/IOB_sent.$g.parser
+    chunk_parser=../tmp/web/chunk.$g.parser
+    clause_parser=../tmp/web/clause.$g.parser
+    chunk_sent_parser=../tmp/web/chunk_sent.$g.parser
+    clause_sent_parser=../tmp/web/clause_sent.$g.parser
 
 
-#     python unit_parser_main.py -baseline -train $train_file $sent_parser
-#     python unit_parser_main.py -IOB -train $train_file $IOB_sent_parser
-#     python unit_parser_main.py -chunk -train $train_file $chunk_parser $chunk_sent_parser
-#     python unit_parser_main.py -clause -train $train_file $clause_parser $clause_sent_parser
+
+    test_file=../tmp/web/$g.cx
+    baseline_output=../tmp/web/$g.baseline.conll06
+    IOB_output=../tmp/web/$g.IOB.conll06
+    chunk_output=../tmp/web/$g.chunk.conll06
+    clause_output=../tmp/web/$g.clause.conll06
+    echo $g
 
 
-#     # baseline
-#     python unit_parser_main.py -baseline -test $test_file $sent_parser $baseline_output
+    python unit_parser_main.py -baseline -train $train_file $sent_parser
+    python unit_parser_main.py -IOB -train $train_file $IOB_sent_parser
+    python unit_parser_main.py -chunk -train $train_file $chunk_parser $chunk_sent_parser
+    python unit_parser_main.py -clause -train $train_file $clause_parser $clause_sent_parser
 
-#     # # baseline + IOB feature
-#     python unit_parser_main.py -IOB -test $test_file $IOB_sent_parser $IOB_output
 
-#     # parse chunk
-#     python unit_parser_main.py -chunk -test $test_file $chunk_parser $chunk_sent_parser $chunk_output 1.3
+    # baseline
+    python unit_parser_main.py -baseline -test $test_file $sent_parser $baseline_output
 
-#     # parse clause
-#     python unit_parser_main.py -clause -test $test_file $clause_parser $clause_sent_parser $clause_output 1.2
+    # # baseline + IOB feature
+    python unit_parser_main.py -IOB -test $test_file $IOB_sent_parser $IOB_output
 
-# done
+    # parse chunk
+    python unit_parser_main.py -chunk -test $test_file $chunk_parser $chunk_sent_parser $chunk_output 1.3
+
+    # parse clause
+    python unit_parser_main.py -clause -test $test_file $clause_parser $clause_sent_parser $clause_output 1.2
+
+done
 
 
 
