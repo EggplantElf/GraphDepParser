724f62d07f3465b0795d4b13d907365017403ee8
Xiang Yu <eggplant.elf@gmail.com>
2014-12-07 15:01:45 +0100

    test factor

--- factors.sh
+++ factors.sh
@@ -3,38 +3,17 @@ gold='../data/english/dev/wsj_dev.conll06'
 train_file='../tmp/wsj_train.fold.cx'
 test_file='../tmp/wsj_dev.cx'
 
-sent_parser='../tmp/sent.fold.parser'
-IOB_sent_parser='../tmp/IOB_test.fold.parser'
-
-baseline_output='../tmp/wsj_dev.fold.baseline.conll06'
-
-
 
 echo run_dev_fold.sh
 
-# # baseline
-# python unit_parser_main.py -baseline -train $train_file $sent_parser
-# python unit_parser_main.py -baseline -test $test_file $sent_parser $baseline_output
+IOB_output=../tmp/wsj_dev.fac.$i.conll06
+IOB_sent_parser=../tmp/fac.$i.parser
 
-# baseline + IOB feature
-for i in 1 2 3 4 5 6 7 8
-do
-    IOB_output=../tmp/wsj_dev.fac.$i.conll06
-    python unit_parser_main.py -fa$i -train $train_file $IOB_sent_parser
-    python unit_parser_main.py -fa$i -test $test_file $IOB_sent_parser $IOB_output
-done
+python unit_parser_main.py -fa$i -train $train_file $IOB_sent_parser
+python unit_parser_main.py -fa$i -test $test_file $IOB_sent_parser $IOB_output
 
+perl eval07.pl -q -p -g $gold -s $IOB_output
 
-# # results
-echo baseline
-perl eval07.pl -q -p -g $gold -s $baseline_output
-
-for i in 1 2 3 4 5 6 7 8
-do
-    echo $i
-    IOB_output=../tmp/wsj_dev.fac.$i.conll06
-    perl eval07.pl -q -p -g $gold -s $IOB_output
-done
 
 
 
